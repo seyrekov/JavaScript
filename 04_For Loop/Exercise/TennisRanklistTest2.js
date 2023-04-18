73eb902100adcc53f92330dbd52f012beaee8c56
seyrekov <r.seyrekov@abv.bg>
2023-04-18 11:54:18 +0300

    modifytest2,3

--- 04_For Loop/Exercise/TennisRanklistTest2.js
+++ 04_For Loop/Exercise/TennisRanklistTest2.js
@@ -5,11 +5,14 @@ describe('tennis', function() {
   it('should return Final points: 6190, Average points: 1360, 50.00% for input 4, 750, SF, W, SF, W', function() {
     const input = ['4', '750', 'SF', 'W', 'SF', 'W'];
     const expectedOutput = 'Final points: 6190\nAverage points: 1360\n50.00%';
+
+    // Capture the console output of the function
     const { stdout } = require('test-console');
     const output = stdout.inspectSync(function() {
-      tennis(input);
+      console.log(tennis(input));
     }).join('\n');
 
+    // Compare the expected and actual output
     assert.strictEqual(output, expectedOutput);
   });
 });
--- 04_For Loop/Exercise/TennisRanklistTest3.js
+++ 04_For Loop/Exercise/TennisRanklistTest3.js
@@ -5,11 +5,11 @@ describe('tennis', function() {
   it('should return Final points: 11040, Average points: 1405, 42.86% for input 7, 1200, SF, F, W, F, W, SF, W', function() {
     const input = ['7', '1200', 'SF', 'F', 'W', 'F', 'W', 'SF', 'W'];
     const expectedOutput = 'Final points: 11040\nAverage points: 1405\n42.86%';
-    const { stdout } = require('test-console');
-    const output = stdout.inspectSync(function() {
-      tennis(input);
-    }).join('\n');
 
+    // Call the function and capture the output
+    const output = tennis(input);
+
+    // Compare the expected and actual output
     assert.strictEqual(output, expectedOutput);
   });
 });
