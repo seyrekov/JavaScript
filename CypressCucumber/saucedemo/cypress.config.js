7c673124da969f1d99255cfb45dfeedfeab526e0
seyrekov <r.seyrekov@abv.bg>
2023-07-10 11:44:16 +0300

    videos

--- CypressCucumber/saucedemo/cypress.config.js
+++ CypressCucumber/saucedemo/cypress.config.js
@@ -1,4 +1,4 @@
-const { defineConfig } = require("cypress");
+/*const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   video: false,
@@ -10,4 +10,27 @@ module.exports = defineConfig({
       // implement node event listeners here
     },
   },
-});
+});*/
+const cucumber = require("cypress-cucumber-preprocessor").default;
+
+module.exports = {
+  ...(on, config) => {
+    on("file:preprocessor", cucumber());
+
+    config.video = false;
+    config.defaultCommandTimeout = 5000;
+    config.pageLoadTimeout = 10000;
+    config.supportFile = "cypress/support/US1-TS1.1-Navigation-HP-SU.steps.js";
+    config.testingType = "e2e";
+    
+    return config;
+  },
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    baseUrl: "https://www.saucedemo.com", // Set the base URL here
+  },
+}
+
